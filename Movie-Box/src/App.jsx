9087569384be95f4b2cd83c481b5d19efabfd8f4
firoz <firozz0007@gmail.com>
2026-08-20 23:47:03 +0530

    its litterally fire going too fast damnn!!!!

--- Movie-Box/src/App.jsx
+++ Movie-Box/src/App.jsx
@@ -1,10 +1,17 @@
 import moviesAndShows from "./Backend/Data";
 import Moviebox from "./Components/Moviebox";
-
+import { useState } from "react";
 function App() {
+  const [search, setSearch] = useState("");
+ const filteredMovies = moviesAndShows.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <>
-      <Moviebox moviesAndShows={moviesAndShows} />
+       <div className="input-div">
+        <input type="text" placeholder="Search the movie..." value={search} onChange={(e) => setSearch(e.target.value)} />
+    </div>
+      <Moviebox moviesAndShows={filteredMovies} />
     </>
   );
 }
